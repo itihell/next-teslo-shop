9908d062fe8e46a75bc6a4751335faeafb329a0d
Itihell Mejia <itihell.mejia@gmail.com>
2024-01-09 11:29:12 -0600

    Agregando fuentes para los titulos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <h1>Hola mundo</h1>
+      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
+      <h1 className={`${titleFont.className}`}>Hola mundo</h1>
     </main>
-  )
+  );
 }
