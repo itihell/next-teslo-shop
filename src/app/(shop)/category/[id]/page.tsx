9801d2abdcbd1ab885e04bb5733922e8ed888a41
Itihell Mejia <itihell.mejia@gmail.com>
2024-01-10 09:44:22 -0600

    Creando la pagina not found de categorias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,18 @@
+import { notFound } from "next/navigation";
 
-export default function CategoryPage() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+  if (id === "kids") {
+    notFound();
+  }
   return (
     <div>
-      <h1>Category PAge</h1>
+      <h1>Category PAge {id}</h1>
     </div>
   );
 }
