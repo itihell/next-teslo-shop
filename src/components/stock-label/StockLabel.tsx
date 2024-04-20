a9e0e9afe94f57906343a08896246f89f7a39198
Itihell Mejia <itihell.mejia@gmail.com>
2024-04-19 18:01:40 -0600

    Mostrando el stock en un componente aparte sin revalidar

--- src/components/stock-label/StockLabel.tsx
+++ src/components/stock-label/StockLabel.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import { getStockBySlug } from "@/actions";
 import { titleFont } from "@/config/fonts";
+import { useEffect, useState } from "react";
 interface Props {
   slug: string;
 }
 export const StockLabel = ({ slug }: Props) => {
+  const [stock, setStock] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getStock();
+  }, []);
+
+  const getStock = async () => {
+    const stock = await getStockBySlug(slug);
+    setStock(stock);
+    setIsLoading(false);
+  };
   return (
-    <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
-      Stock: 150
+    <h1 className={`${titleFont.className} antialiased font-bold text-xg`}>
+      Stock: {isLoading ? "Cargando..." : stock}
     </h1>
   );
 };
