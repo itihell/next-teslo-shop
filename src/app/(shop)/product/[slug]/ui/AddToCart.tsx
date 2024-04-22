664ad27647ae7effea64f1e88606e54594850929
Itihell Mejia <itihell.mejia@gmail.com>
2024-04-22 11:50:02 -0600

    Separando la funcionalidad de agregar al carrito de compras

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,23 +1,34 @@
+"use client";
+
 import { QuantitySelector, SizeSelector } from "@/components";
-import { Product } from "@/interfaces";
+import { Product, Size } from "@/interfaces";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
-export const AddToCart = ({product }:Props) => {
+export const AddToCart = ({ product }: Props) => {
+  const [size, setSize] = useState<Size | undefined>();
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const addToCart = () => {
+    console.log(size, quantity);
+  };
+
   return (
     <>
       {/* Selector de tallas */}
       <SizeSelector
         availableSizes={product.sizes}
-        selectedSize={product.sizes[0]}
+        selectedSize={size}
+        onSizeChange={setSize}
       />
       {/* Selector de cantidad */}
-      <QuantitySelector quantity={0} />
+      <QuantitySelector quantity={quantity} onQuantityChange={setQuantity} />
 
       {/* Boton */}
-      <button className="btn-primary my-5">Agregar al carrito</button>
+      <button onClick={addToCart} className="btn-primary my-5">Agregar al carrito</button>
     </>
   );
 };
