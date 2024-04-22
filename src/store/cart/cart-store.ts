deb95037626acc271e96db8dcb4add5aec094a15
Itihell Mejia <itihell.mejia@gmail.com>
2024-04-22 14:19:11 -0600

    Persistir estado en el localstorage

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -12,31 +12,38 @@ interface State {
 }
 
 // Creando el store de zustand
-export const useCartStore = create<State>((set, get) => ({
-  cart: [],
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({
+      cart: [],
 
-  // Metodos
-  addProductToCart: (product: CartProduct) => {
-    const { cart } = get();
+      // Metodos
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
 
-    // Verificar si el producto ya esta en el carrito
-    const productInCart = cart.some(
-      (item) => item.id === product.id && item.size === product.size
-    );
+        // Verificar si el producto ya esta en el carrito
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
 
-    if (!productInCart) {
-      set({ cart: [...cart, product] });
-      return;
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
+        }
+
+        // Si el producto existe por talla, se actualiza la cantidad
+        const updateCartProduct = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+          return item;
+        });
+
+        set({ cart: updateCartProduct });
+      },
+    }),
+    {
+      name: "shopping-cart",
     }
-
-    // Si el producto existe por talla, se actualiza la cantidad
-    const updateCartProduct = cart.map((item) => {
-      if (item.id === product.id && item.size === product.size) {
-        return { ...item, quantity: item.quantity + product.quantity };
-      }
-      return item;
-    });
-
-    set({ cart: updateCartProduct });
-  },
-}));
+  )
+);
